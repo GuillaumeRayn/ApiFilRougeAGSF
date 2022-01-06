6f9d2d1f13c86ce4a6d74ef739e569c4965fdcf2
[GuillaumeRayn] <guillaumeraynard.pro@gmail.com>
2022-01-06 10:52:43 +0100

    first commit

---
+++ FilRouge/Tools/DataContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace FilRouge.Tools
+{
+    public class DataContext : DbContext
+    {
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=(LocalDb)\coursEF;Integrated Security=True");
+        }
+    }
+}
