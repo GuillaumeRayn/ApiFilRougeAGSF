24e4c7a1faa749301e62e63b9e80dbbf3828e2f8
[GuillaumeRayn] <guillaumeraynard.pro@gmail.com>
2022-01-07 10:45:27 +0100

    ajout user controller

---
+++ FilRouge/Controllers/UserController.cs
@@ -0,0 +1,30 @@
+﻿using FilRouge.Interfaces;
+using FilRouge.Model;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FilRouge.Controllers
+{
+    [Route("api/v1/utilisateurs")]
+    public class UserController : Controller
+    {
+        IRepository<User> _userRepository;
+        LoginService _loginService;
+
+        public UserController(IRepository<User> repository, LoginService loginService)
+        {
+            _userRepository = repository;
+            _loginService = loginService;
+        }
+
+        [HttpPost]
+        public IActionResult SubmitFormLogin([FromForm] string login, [FromForm] string password)
+        {
+            string token = _loginService.GenerateToken(login, password);
+            if (token != null)
+            {
+                return Ok(new { Token = token });
+            }
+            return NotFound();
+        }
+    }
+}
