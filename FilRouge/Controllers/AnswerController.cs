1d65c03bdd7938c0bb4cac4c7321ea04a06a66a4
[GuillaumeRayn] <guillaumeraynard.pro@gmail.com>
2022-01-07 11:41:11 +0100

    ajout question controller

---
+++ FilRouge/Controllers/AnswerController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FilRouge.Controllers
+{
+    [Route("api/v1/questions")]
+    public class AnswerController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ FilRouge/Controllers/QuestionController.cs
@@ -0,0 +1,35 @@
+﻿using FilRouge.Interfaces;
+using FilRouge.Model;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FilRouge.Controllers
+{
+    [Route("api/v1/questions")]
+    public class QuestionController : Controller
+    {
+
+        IRepository<Question> _questionRepository;
+        
+        public QuestionController(IRepository<Question> repository)
+        {
+            _questionRepository = repository;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_questionRepository.GetAll());
+        }
+
+        [HttpGet("{questionId}")]
+        public IActionResult Get(int questionId)
+        {
+            Question q = _questionRepository.Get(questionId);
+            if (q != null)
+            {
+                return Ok(q);
+            }
+            return NotFound();
+        }
+    }
+}
